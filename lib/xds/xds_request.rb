f227f7e477ae91e5e6e3b06f5fcf134c0b6ae67d
Andy Gregorowicz <andrewg@mitre.org>
2009-02-16 20:35:16 +0000

    Updating to execute the query against the NIST Registry

--- lib/xds/xds_request.rb
+++ lib/xds/xds_request.rb
@@ -20,7 +20,7 @@ module XDS
      end
      
      def to_soap(builder)
-       builder.Header("xmlns"=>"http://www.w3.org/2003/05/soap-envelope","xmlns:wsa" =>"http://www.w3.org/2005/08/addressing") do
+       builder.Header() do
           builder.wsa(:To, @endpoint_uri)
           builder.wsa(:Action, @action)
           builder.wsa(:MessageID, @message_id) 
@@ -33,12 +33,22 @@ module XDS
     attr_reader :header
     attr_accessor :endpoint_uri  
     
-    def to_soap(builder = Builder::XmlMarkup.new(:indent => 2),attributes={})
-        header.to_soap(builder,endpoint_uri)
-        to_soap_body(builder,attributes)
+    def execute
+      client = DefaultHttpClient.new
+      post = HttpPost.new(endpoint_uri)
+      post.entity = StringEntity.new(to_soap)
+      client.execute(post)
     end
     
-    def to_saop_body(builder,attributes={})
+    def to_soap(builder = Builder::XmlMarkup.new(:indent => 2),attributes={})
+      builder.soapenv(:Envelope, "xmlns"=>"http://www.w3.org/2003/05/soap-envelope","xmlns:wsa" =>"http://www.w3.org/2005/08/addressing") do
+        header.endpoint_uri = endpoint_uri
+        header.to_soap(builder)
+        to_soap_body(builder, attributes)
+      end
+    end
+    
+    def to_soap_body(builder,attributes={})
       raise "Implement Me, Really!!!!"
     end
   end 
