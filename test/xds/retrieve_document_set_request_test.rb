4c9d706cade027ca2ee74d7a8625d54c9b12e759
Andy Gregorowicz <andrewg@mitre.org>
2009-02-17 21:07:19 +0000

    Changing the document id's in the test so that they match ones in the NIST public registry

--- test/xds/retrieve_document_set_request_test.rb
+++ test/xds/retrieve_document_set_request_test.rb
@@ -6,16 +6,16 @@ class RetrieveDocumentSetRequestTest < Test::Unit::TestCase
     setup do
      
       @rdsr = XDS::RetrieveDocumentSetRequest.new("http://129.6.24.109:9080/axis2xop/services/xdsrepositoryb")
-      @rdsr.add_ids_to_request('1.19.6.24.109.42.1', '129.6.58.91.13297')
-      @rdsr.add_ids_to_request('1.19.6.24.109.42.1', '129.6.58.91.13298')
+      @rdsr.add_ids_to_request('1.19.6.24.109.42.1', '229.6.58.29.939')
+      @rdsr.add_ids_to_request('1.19.6.24.109.42.1', '229.6.58.29.937')
     end
     
     should "create a SOAP Body" do
       soap_body = @rdsr.to_soap_body(create_builder,'xmlns:soapenv' => "http://www.w3.org/2003/05/soap-envelope")
       assert soap_body
       assert_xpath(soap_body, "/soapenv:Body/xdsb:RetrieveDocumentSetRequest/xdsb:DocumentRequest/xdsb:RepositoryUniqueId[text()='1.19.6.24.109.42.1']", common_namespaces, 2)
-      assert_xpath(soap_body, "/soapenv:Body/xdsb:RetrieveDocumentSetRequest/xdsb:DocumentRequest/xdsb:DocumentUniqueId[text()='129.6.58.91.13297']", common_namespaces)
-      assert_xpath(soap_body, "/soapenv:Body/xdsb:RetrieveDocumentSetRequest/xdsb:DocumentRequest/xdsb:DocumentUniqueId[text()='129.6.58.91.13298']", common_namespaces)
+      assert_xpath(soap_body, "/soapenv:Body/xdsb:RetrieveDocumentSetRequest/xdsb:DocumentRequest/xdsb:DocumentUniqueId[text()='229.6.58.29.939']", common_namespaces)
+      assert_xpath(soap_body, "/soapenv:Body/xdsb:RetrieveDocumentSetRequest/xdsb:DocumentRequest/xdsb:DocumentUniqueId[text()='229.6.58.29.937']", common_namespaces)
     end
     
     should "query the NIST Public Registry" do
