263fc01d4f74538fc89164a6c49c62c75fee7963
Rob Dingwell <bobd@mitre.org>
2009-02-18 18:44:45 +0000

    fixing typo

--- lib/xds/provide_and_register_document_set_b.rb
+++ lib/xds/provide_and_register_document_set_b.rb
@@ -1,7 +1,7 @@
 class ProvideAndRegisterDocumentSetB
   
     def initialize(service_url)
-        super(serive_url,"urn:ihe:iti:2007:ProvideAndRegisterDocumentSet-b")
+        super(service_url,"urn:ihe:iti:2007:ProvideAndRegisterDocumentSet-b")
     end
   
 end
