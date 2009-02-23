1c8fca7953139d447e86d49aaadc5c01a6983e72
Andy Gregorowicz <andrewg@mitre.org>
2009-02-23 22:08:35 +0000

    implementing the registery_stored_query method on DocumentRegistry

--- lib/xds/document_registry_service.rb
+++ lib/xds/document_registry_service.rb
@@ -1,3 +1,4 @@
+# Provides an interface to an XDS Document Registry
 class DocumentRegistry
   
   
@@ -9,8 +10,16 @@ class DocumentRegistry
   
   end
   
-  def registery_stored_query
-    
+  # Issues a stored query request to the XDS Document Registry
+  # Will return an Array of XDS::Metadata
+  # <tt>query</tt> - a Hash where the key is the parameter name and value is the value to query against
+  # call-seq:
+  # dr = DocumentRegistry.new(http://yourregistry.com/xdsb)
+  # dr.registery_stored_query({"$XDSDocumentEntryPatientId" => "'93f3f8a6d100463^^^&1.3.6.1.4.1.21367.2005.3.7&ISO'",
+  #                            "$XDSDocumentEntryStatus" => "('urn:oasis:names:tc:ebxml-regrep:StatusType:Approved')"})
+  def registery_stored_query(query)
+    rsqr = XDS::RegistryStoredQueryRequest.new(@service_url, query)
+    rsqr.retrieved_metadata
   end
   
   
